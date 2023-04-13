cbda3ab8b2d026edb59302a86dadb6640db1fb72
Hugh Zheng <95636804+HUGHHHHHHHHHH@users.noreply.github.com>
2023-04-13 22:38:13 +0800

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,5 +1,5 @@
 import pygame
-from dbcontrol import DBControl
+from DBControl import DBControl
 
 # skin status
 UNPOSSESSED = 0
@@ -14,7 +14,7 @@ class Shop:
     skin_name = ['Glory', 'King', 'IDK']
     buff_name = ['Extra life', 'Score multiplier', 'Speed booster', 'Shield', 'Ghost slower']
 
-    def __init__(self, dbcontrol):
+    def __init__(self, DBControl):
         # buff0: extra life
         # buff1: score multiplier
         # buff2: pacman speed booster
@@ -22,7 +22,7 @@ class Shop:
         # buff4: ghost slow
 
         # for later call of dbcontrol and main
-        self.dbcontrol = dbcontrol
+        self.DBControl = DBControl
 
 
     # upgrade buff function is to upgrade designated buff by one level each time
@@ -34,19 +34,19 @@ class Shop:
         MAX_LEVEL = 5
         message = ""
 
-        if 0 <= buff_idx < len(self.dbcontrol.buff):
-            current_buff_level = self.dbcontrol.buff[buff_idx]
+        if 0 <= buff_idx < len(self.DBControl.buff):
+            current_buff_level = self.DBControl.buff[buff_idx]
             if current_buff_level < MAX_LEVEL:
                 buff_upgrade_cost = (current_buff_level + 1) * 100
 
-                if self.dbcontrol.gold >= buff_upgrade_cost:
-                    self.dbcontrol.gold -= buff_upgrade_cost
-                    self.dbcontrol.buff[buff_idx] += 1
+                if self.DBControl.gold >= buff_upgrade_cost:
+                    self.DBControl.gold -= buff_upgrade_cost
+                    self.DBControl.buff[buff_idx] += 1
                     message = f"{self.buff_name[buff_idx]} upgraded to level {current_buff_level + 1}."
                 else:
                     message = "Insufficient gold."
             else:
-                message = f"{self.dbcontrol.buff[buff_idx]} is already at the maximum level."
+                message = f"{self.DBControl.buff[buff_idx]} is already at the maximum level."
         else:
             message = "Invalid buff index."
         return message
@@ -59,17 +59,17 @@ class Shop:
         default_skin_price = 500
         message = ""
 
-        if 0 <= skin_idx < len(self.dbcontrol.skin):
-            if self.dbcontrol.skin[skin_idx] == UNPOSSESSED: 
+        if 0 <= skin_idx < len(self.DBControl.skin):
+            if self.DBControl.skin[skin_idx] == UNPOSSESSED: 
 
                 if self.skin_price[skin_idx] == 0: # default case, see __init__
                     current_skin_price = default_skin_price
                 else:
                     current_skin_price = self.skin_price[skin_idx]
 
-                if self.dbcontrol.gold >= current_skin_price:
-                    self.dbcontrol.gold -= current_skin_price
-                    self.dbcontrol.skin[skin_idx] = POSSESSED
+                if self.DBControl.gold >= current_skin_price:
+                    self.DBControl.gold -= current_skin_price
+                    self.DBControl.skin[skin_idx] = POSSESSED
                     message = "Purchase successful"
                 else:
                     message = "Insufficient gold."
@@ -85,16 +85,16 @@ class Shop:
     def equip_skin(self, skin_idx):
 
         message = ""
-        if 0 <= skin_idx < len(self.dbcontrol.skin):
-            if self.dbcontrol.skin[skin_idx] == POSSESSED:
+        if 0 <= skin_idx < len(self.DBControl.skin):
+            if self.DBControl.skin[skin_idx] == POSSESSED:
                 # unequip the currently equipped skin
-                for i in range(len(self.dbcontrol.skin)):
-                    if self.dbcontrol.skin[i] == EQUIPPED:
-                        self.dbcontrol.skin[i] == POSSESSED
+                for i in range(len(self.DBControl.skin)):
+                    if self.DBControl.skin[i] == EQUIPPED:
+                        self.DBControl.skin[i] == POSSESSED
                 # equip the current skin
-                self.dbcontrol.skin[skin_idx] = EQUIPPED             
+                self.DBControl.skin[skin_idx] = EQUIPPED             
                 message = f"{self.skin_name[skin_idx]} equipped."
-            elif self.dbcontrol.skin[skin_idx] == EQUIPPED:
+            elif self.DBControl.skin[skin_idx] == EQUIPPED:
                 message = "You have already equipped the skin."
             else:
                 message = "You do not own this skin."
@@ -104,3 +104,10 @@ class Shop:
     
     def get_skin_name(self, skin_idx):
         return self.skin_name[skin_idx]
+    
+    def draw_gold_balance(self):
+        gold_balance_text = f"Gold: {self.DBControl.gold}"
+        gold_balance_font = pygame.font.Font(None, 20)
+        gold_balance_surf = gold_balance_font.render(gold_balance_text, True, (255, 255, 255))
+        gold_balance_rect = gold_balance_surf.get_rect(topright=(self.main.screen.get_width() - 20, 20))
+        self.main.screen.blit(gold_balance_surf, gold_balance_rect)
